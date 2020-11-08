d04de37b44d58a40b84b56afc51eb3c522ac70d3
Denis Kulbachniy <Denismerefa@gmail.com>
2020-11-08 13:24:54 +0200

    created task AT-7

---
+++ src/main/java/taskAT7/Methods.java
@@ -0,0 +1,19 @@
+package taskAT7;
+
+public class Methods {
+    public static void add ( int operand1, int operand2){
+        System.out.println(operand1 + " + " + operand2 + " = " + (operand1 + operand2));
+    }
+
+    public static void sub ( int operand1, int operand2){
+        System.out.println(operand1 + " - " + operand2 + " = " + (operand1 - operand2));
+    }
+
+    public static void mul ( int operand1, int operand2){
+        System.out.println(operand1 + " * " + operand2 + " = " + (operand1 * operand2));
+    }
+
+    public static void div ( int operand1, int operand2){
+        System.out.println(operand1 + " / " + operand2 + " = " + (operand1 / operand2));
+    }
+}
